5ddad74e1c6089ee79733ceaf3d38cdf796033ab
V.lud <lakal64@yandex.ru>
2022-07-15 16:24:57 +0300

    electo cards done

---
+++ src/components/cardElectro/CardElectro.jsx
@@ -0,0 +1,30 @@
+import './CardElectro.css'
+
+export default function CardElectro(props) {
+    const { title, description, list, logo } = props.data;
+
+    return (
+        <div className='electro__card'>
+            <div className='card__logo'>
+                <img src={logo} alt="" />
+            </div>
+            <div className='card__info'>
+                <div className='info__title'>
+                    {title}
+                </div>
+                <div className='info__desc'>
+                    {description}
+                </div>
+                <div className='info__list'>
+                    <ul className='list__roster'>
+                        {list.map(para =>
+                            <li className='roster__para'>
+                                {para}
+                            </li>
+                        )}
+                    </ul>
+                </div>
+            </div>
+        </div>
+    )
+}
--- src/pages/profileMain/ProfileMain.jsx
+++ src/pages/profileMain/ProfileMain.jsx
@@ -1,11 +1,78 @@
+import CardElectro from '../../components/cardElectro/CardElectro'
 import './ProfileMain.css'
+import infoLogo from '../../images/report (2) 1.svg'
+import resultLogo from '../../images/lab 1.svg'
+import addLogo from '../../images/add (1) 1.svg'
+import historyLogo from '../../images/clock 1.svg'
 
-export default function ProfileMain () {
+export default function ProfileMain() {
 
+    let electroCardData = [
+        {
+            logo: infoLogo,
+            title: 'Информация о пациенте',
+            description: 'Просмотрите интересующую вас информацию о своем здоровье',
+            list: [
+                'Ваши личные данные',
+                'Рекомендации врачей',
+                'История болезней'
+            ],
+        },
+        {
+            logo: resultLogo,
+            title: 'Результаты анализов',
+            description: 'Вы можете узнать здесь результаты своих анализов',
+            list: [
+                'Ваши личные данные',
+                'Рекомендации врачей',
+                'История болезней'
+            ],
+        },
+        {
+            logo: addLogo,
+            title: 'Добавить  информацию',
+            description: 'Добавляйте в свою электронную медицинскую карту новые данные',
+            list: [
+                'Ваши личные данные',
+                'Рекомендации врачей',
+                'История болезней'
+            ],
+        },
+        {
+            logo: historyLogo,
+            title: 'История приемов',
+            description: 'Вся информация о полученных услугах за все время хранится здесь',
+            list: [
+                'Ваши личные данные',
+                'Рекомендации врачей',
+                'История болезней'
+            ],
+        },
+    ];
 
     return (
-        <>
-        
-        </>
+        <div className='main__info'>
+            <article>
+                <p>
+                    Записи на прием
+                </p>
+                <div>
+
+                </div>
+            </article>
+
+            <article className='info__electro'>
+                <p className='electro__title'>
+                    Электронная карта
+                </p>
+                <div className='electro__cards'>
+                    {
+                        electroCardData.map(card =>
+                            <CardElectro data={card} key={card.title} />
+                        )
+                    }
+                </div>
+            </article>
+        </div>
     )
 }
